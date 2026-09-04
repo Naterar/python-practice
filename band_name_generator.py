b8d655a18e748a0d0c5439a0bac5e2e8c59dfdee
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2026-09-03 22:30:20 -0400

    Band name generator project.

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Hello to the greatest Band Name Generator")
+name_of_city = input("Which city are you from: ")
+pets_name = input("What's the name of your pet? ")
+print(f"Your band name should be {name_of_city} {pets_name}")
