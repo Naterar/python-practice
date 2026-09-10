0fe9c256445a05aa386f90ca6fb7df6ecda51abd
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2026-09-09 20:03:44 -0400

    Add F1 driver nickname generator

---
+++ f1_race_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the F1 Race Name Generator!")
+driver_name = input("What is your name? ")
+driver_team = input("What is your team name? ")
+driver_nationality = input("What is your nationality? ")
+driver_nickname = driver_name + " " + driver_team
+print(f"{driver_name}, from {driver_nationality}; your F1 driver nickname is: {driver_nickname}")
