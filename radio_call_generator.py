131388d49e6d6fcdac9260363d4c5f994eac8425
Roberto Natera <ohana2116@gmail.com>
2026-09-18 21:48:36 -0400

    Day 6: funtion definitions and calls - radio call generator

---
+++ radio_call_generator.py
@@ -0,0 +1,29 @@
+def startup_call():
+    print("Area clear, door secure.")
+    print("Safety pins removed and stored.")
+    print("Ground crew ready.")
+
+
+def taxi_call():
+    print("Call ground for exiting the ramp.")
+    print("Ground crew ready and clear")
+    print("Chocks off and brake off")
+    print("Brake checks and thrust reverses")
+
+
+def takeoff_call():
+    print("Run-up check good")
+    print("Seatbelt sign on, cabin crew ready for takeoff")
+    print("Contact tower for takeoff")
+
+
+def departure_call():
+    print("Tower, claiming to 3000ft at 1000")
+    print("Ready to turn base")
+    print("Proceed VFR to Bahamas.")
+
+
+startup_call()
+taxi_call()
+takeoff_call()
+departure_call()
