19f4e47fd018a057b104618733a26eb51fea5354
Roberto Natera <ohana2116@gmail.com>
2026-09-24 10:16:31 -0400

    fix temperature threshold to 113F

--- f1_tyre_strategy_advisor.py
+++ f1_tyre_strategy_advisor.py
@@ -4,7 +4,7 @@ tyre_age = float(input('Current tyre age in laps: '))
 
 if tyre_age > 25:
     print('BOX BOX BOX.')
-elif track_temp > 145 and tyre_age > 18:
+elif track_temp > 113 and tyre_age > 18:
     print('Box now, new strategy; we are taking mediums.')
 elif 10 < laps_remaining < 20:
     print('Consider a short stint on softs.')
